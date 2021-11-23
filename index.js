65903612c583e5eaf77165396924cfc2bd4e9c5f
QuentinChevaux <quentin.chevaux@gmail.com>
2021-11-23 15:30:05 +0100

    API Pokemon

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ $('#berries').click( () => {
         if ( data.next !== null) {
 
                 $('.next').css('display', 'block')
-                $('.next').attr('onclick', 'clicknext("' +data.next+ '")')
+                $('.next').attr('onclick', 'clicknext("' +data.next+ '","more_info_berry")')
 
             }
 
@@ -33,7 +33,7 @@ $('#berries').click( () => {
         if ( data.previous !== null) {
 
                 $('.previous').css('display', 'block')
-                $('.previous').attr('onclick', 'clickprevious("' +data.previous+ '")')
+                $('.previous').attr('onclick', 'clickprevious("' +data.previous+ '","more_info_berry")')
 
             }
 
@@ -87,7 +87,7 @@ $('#contest').click( () => {
         if ( data.next !== null) {
 
                 $('.next').css('display', 'block')
-                $('.next').attr('onclick', 'clicknext("' +data.next+ '")')
+                $('.next').attr('onclick', 'clicknext("' +data.next+ '","more_info_contest")')
 
             }
 
@@ -100,7 +100,7 @@ $('#contest').click( () => {
         if ( data.previous !== null) {
 
                 $('.previous').css('display', 'block')
-                $('.previous').attr('onclick', 'clickprevious("' +data.previous+ '")')
+                $('.previous').attr('onclick', 'clickprevious("' +data.previous+ '","more_info_contest")')
 
             }
 
@@ -152,7 +152,7 @@ $('#encounters').click( () => {
         if ( data.next !== null) {
 
                 $('.next').css('display', 'block')
-                $('.next').attr('onclick', 'clicknext("' +data.next+ '")')
+                $('.next').attr('onclick', 'clicknext("' +data.next+ '","more_info_encounters")')
 
             }
 
@@ -165,7 +165,7 @@ $('#encounters').click( () => {
         if ( data.previous !== null) {
 
                 $('.previous').css('display', 'block')
-                $('.previous').attr('onclick', 'clickprevious("' +data.previous+ '")')
+                $('.previous').attr('onclick', 'clickprevious("' +data.previous+ '","more_info_encounters")')
 
             }
 
@@ -188,8 +188,6 @@ function more_info_encounters(url) {
     $.get(url, function(data) {
 
         $('#div_2').html( $('#div_2').html() + "Methode pour lancer un Combat : " + data.name + '<br />')
-
-        //$('#div_2').html( $('#div_2').html() + "Methode pour lancer un Combat : " + data.names. + '<br />')
 
     })
 
@@ -217,7 +215,7 @@ $('#evolution').click( () => {
         if ( data.next !== null) {
 
                 $('.next').css('display', 'block')
-                $('.next').attr('onclick', 'clicknext("' +data.next+ '")')
+                $('.next').attr('onclick', 'clicknext("' +data.next+ '","more_info_evolutions")')
 
             }
 
@@ -230,7 +228,7 @@ $('#evolution').click( () => {
         if ( data.previous !== null) {
 
                 $('.previous').css('display', 'block')
-                $('.previous').attr('onclick', 'clickprevious("' +data.previous+ '")')
+                $('.previous').attr('onclick', 'clickprevious("' +data.previous+ '","more_info_evolutions")')
 
             }
 
@@ -284,7 +282,7 @@ $('#games').click( () => {
         if ( data.next !== null) {
 
                 $('.next').css('display', 'block')
-                $('.next').attr('onclick', 'clicknext("' +data.next+ '")')
+                $('.next').attr('onclick', 'clicknext("' +data.next+ '","more_info_generation")')
 
             }
 
@@ -297,7 +295,7 @@ $('#games').click( () => {
         if ( data.previous !== null) {
 
                 $('.previous').css('display', 'block')
-                $('.previous').attr('onclick', 'clickprevious("' +data.previous+ '")')
+                $('.previous').attr('onclick', 'clickprevious("' +data.previous+ '","more_info_generation")')
 
             }
 
@@ -325,10 +323,347 @@ function more_info_generation(url) {
 
 }
 
+                                                                    // BOUTON ITEMS
+
+$('#items').click( () => {
+
+    $('#div_1').css('display', 'flex')
+
+    $.get('https://pokeapi.co/api/v2/item/', function(data) {
+        
+        $('#pokemon_id').html('')
+        $('#pokemon_name').html('')
+
+        for ( i = 0; i < data.results.length; i++) {
+
+            $('#pokemon_id').html( $('#pokemon_id').html() + '<br />' + "<div class='div_hard_berry' onclick='more_info_items(\" " + data.results[i].url + " \")'>" + '#' + (i + 1) +  '</div>')
+
+            $('#pokemon_name').html( $('#pokemon_name').html() + '<br />' + "<div class='div_hard_berry' onclick='more_info_items(\" " + data.results[i].url + " \")'>" + data.results[i].name + '</div>')
+
+        }
+        
+        if ( data.next !== null) {
+
+                $('.next').css('display', 'block')
+                $('.next').attr('onclick', 'clicknext("' +data.next+ '","more_info_items")')
+
+            }
+
+        else {
+
+                $('.next').css('display', 'none') 
+
+            }
+
+        if ( data.previous !== null) {
+
+                $('.previous').css('display', 'block')
+                $('.previous').attr('onclick', 'clickprevious("' +data.previous+ '","more_info_items")')
+
+            }
+
+        else {
+
+                $('.previous').css('display', 'none') 
+
+            }
+
+    })
+
+})
+
+function more_info_items(url) {
+
+    $('#div_2').css('display', 'flex')
+
+    $('#div_2').html('')
+
+    $.get(url, function(data) {
+
+        $('#div_2').html( $('#div_2').html() + "Description : " + data.effect_entries[0].effect + '<br />')
+
+        // $('#div_2').html( $('#div_2').html() + "Description : " + data.effect_entries.effect + '<br />')
+
+    })
+
+}
+
+                                                                    // BOUTON LOCATIONS
+
+$('#locations').click( () => {
+
+    $('#div_1').css('display', 'flex')
+
+    $.get('https://pokeapi.co/api/v2/location/', function(data) {
+        
+        $('#pokemon_id').html('')
+        $('#pokemon_name').html('')
+
+        for ( i = 0; i < data.results.length; i++) {
+
+            $('#pokemon_id').html( $('#pokemon_id').html() + '<br />' + "<div class='div_hard_berry' onclick='more_info_location(\" " + data.results[i].url + " \")'>" + '#' + (i + 1) +  '</div>')
+
+            $('#pokemon_name').html( $('#pokemon_name').html() + '<br />' + "<div class='div_hard_berry' onclick='more_info_location(\" " + data.results[i].url + " \")'>" + data.results[i].name + '</div>')
+
+        }
+        
+        if ( data.next !== null) {
+
+                $('.next').css('display', 'block')
+                $('.next').attr('onclick', 'clicknext("' +data.next+ '","more_info_location")')
+
+            }
+
+        else {
+
+                $('.next').css('display', 'none') 
+
+            }
+
+        if ( data.previous !== null) {
+
+                $('.previous').css('display', 'block')
+                $('.previous').attr('onclick', 'clickprevious("' +data.previous+ '","more_info_location")')
+
+            }
+
+        else {
+
+                $('.previous').css('display', 'none') 
+
+            }
+
+    })
+
+})
+
+function more_info_location(url) {
+
+    $('#div_2').css('display', 'flex')
+
+    $('#div_2').html('')
+
+    $.get(url, function(data) {
+
+        $('#div_2').html( $('#div_2').html() + "Area : " + data.areas[0].name + '<br />')
+
+    })
+
+}
+
+                                                                    // BOUTON MACHINES
+
+$('#machines').click( () => {
+
+    $('#div_1').css('display', 'flex')
+
+    $.get('https://pokeapi.co/api/v2/machine/', function(data) {
+        
+        $('#pokemon_id').html('')
+        $('#pokemon_name').html('')
+
+        for ( i = 0; i < data.results.length; i++) {
+
+            $('#pokemon_id').html( $('#pokemon_id').html() + '<br />' + "<div class='div_hard_berry' onclick='more_info_machines(\" " + data.results[i].url + " \")'>" + '#' + (i + 1) +  '</div>')
+
+            $('#pokemon_name').html( $('#pokemon_name').html() + '<br />' + "<div class='div_hard_berry' onclick='more_info_machines(\" " + data.results[i].url + " \")'>" + data.results[i].url + '</div>')
+
+        }
+        
+        if ( data.next !== null) {
+
+                $('.next').css('display', 'block')
+                $('.next').attr('onclick', 'clicknext("' +data.next+ '","more_info_machines")')
+
+            }
+
+        else {
+
+                $('.next').css('display', 'none') 
+
+            }
+
+        if ( data.previous !== null) {
+
+                $('.previous').css('display', 'block')
+                $('.previous').attr('onclick', 'clickprevious("' +data.previous+ '","more_info_machines")')
+
+            }
+
+        else {
+
+                $('.previous').css('display', 'none') 
+
+            }
+
+    })
+
+})
+
+function more_info_machines(url) {
+
+    $('#div_2').css('display', 'flex')
+
+    $('#div_2').html('')
+
+    $.get(url, function(data) {
+
+        $('#div_2').html( $('#div_2').html() + "Item : " + data.item.name + '<br />')
+
+        $('#div_2').html( $('#div_2').html() + "Item : " + data.move.name + '<br />')
+
+        $('#div_2').html( $('#div_2').html() + "Item : " + data.version_group.name + '<br />')
+
+    })
+
+}
+
+                                                                    // BOUTON MOVES
+
+$('#moves').click( () => {
+
+    $('#div_1').css('display', 'flex')
+
+    $.get('https://pokeapi.co/api/v2/move/', function(data) {
+        
+        $('#pokemon_id').html('')
+        $('#pokemon_name').html('')
+
+        for ( i = 0; i < data.results.length; i++) {
+
+            $('#pokemon_id').html( $('#pokemon_id').html() + '<br />' + "<div class='div_hard_berry' onclick='more_info_moves(\" " + data.results[i].url + " \")'>" + '#' + (i + 1) +  '</div>')
+
+            $('#pokemon_name').html( $('#pokemon_name').html() + '<br />' + "<div class='div_hard_berry' onclick='more_info_moves(\" " + data.results[i].url + " \")'>" + data.results[i].name + '</div>')
+
+        }
+        
+        if ( data.next !== null) {
+
+                $('.next').css('display', 'block')
+                $('.next').attr('onclick', 'clicknext("' +data.next+ '","more_info_moves")')
+
+            }
+
+        else {
+
+                $('.next').css('display', 'none') 
+
+            }
+
+        if ( data.previous !== null) {
+
+                $('.previous').css('display', 'block')
+                $('.previous').attr('onclick', 'clickprevious("' +data.previous+ '","more_info_moves")')
+
+            }
+
+        else {
+
+                $('.previous').css('display', 'none') 
+
+            }
+
+    })
+
+})
+
+function more_info_moves(url) {
+
+    $('#div_2').css('display', 'flex')
+
+    $('#div_2').html('')
+
+    $.get(url, function(data) {
+
+        $('#div_2').html( $('#div_2').html() + "Precision : " + data.accuracy + '<br />')
+
+        $('#div_2').html( $('#div_2').html() + "Effect : " + data.effect_entries[0].effect + '<br />')
+
+    })
+
+}
+
+                                                                    // BOUTON POKEMON
+
+$('#pokemon').click( () => {
+
+    $('#div_1').css('display', 'flex')
+
+    $.get('https://pokeapi.co/api/v2/ability/', function(data) {
+        
+        $('#pokemon_id').html('')
+        $('#pokemon_name').html('')
+
+        for ( i = 0; i < data.results.length; i++) {
+
+            $('#pokemon_id').html( $('#pokemon_id').html() + '<br />' + "<div class='div_hard_berry' onclick='more_info_pokemon(\" " + data.results[i].url + " \")'>" + '#' + (i + 1) +  '</div>')
+
+            $('#pokemon_name').html( $('#pokemon_name').html() + '<br />' + "<div class='div_hard_berry' onclick='more_info_pokemon(\" " + data.results[i].url + " \")'>" + data.results[i].name + '</div>')
+
+        }
+        
+        if ( data.next !== null) {
+
+                $('.next').css('display', 'block')
+                $('.next').attr('onclick', 'clicknext("' +data.next+ '","more_info_pokemon")')
+
+            }
+
+        else {
+
+                $('.next').css('display', 'none') 
+
+            }
+
+        if ( data.previous !== null) {
+
+                $('.previous').css('display', 'block')
+                $('.previous').attr('onclick', 'clickprevious("' +data.previous+ '","more_info_pokemon")')
+
+            }
+
+        else {
+
+                $('.previous').css('display', 'none') 
+
+            }
+
+    })
+
+})
+
+function more_info_pokemon(url) {
+
+    $('#div_2').css('display', 'flex')
+
+    $('#div_2').html('')
+
+    $.get(url, function(data) {
+
+        for (i=0; i < data.effect_changes[0].effect_entries.length; i++) {
+
+       if (data.effect_changes[0].effect_entries[i].language.name == "en") {
+
+            $('#div_2').html( $('#div_2').html() + "Precision : " + data.effect_changes[0].effect_entries[i].effect + '<br />')
+
+            $('#div_2').html( $('#div_2').html() + "Effect : " + data.effect_entries[i].effect + '<br />')
+
+
+       }
+    }
+
+    })
+
+}
+
+
+
+
+
 
 // FUNCTION DE BOUTON NEXT ET BOUTON PREVIOUS
 
-function clicknext(url) {
+function clicknext(url,nomFonction) {
 
     console.log(url)
 
@@ -355,16 +690,16 @@ function clicknext(url) {
 
             // console.log(data.results[i])
 
-            $('#pokemon_id').html( $('#pokemon_id').html() + '<br />' + "<div class='div_hard_berry' onclick='more_info_berry(\" " + data.results[i].url + " \")'>" + '#' + (i + 1) +  '</div>')
+            $('#pokemon_id').html( $('#pokemon_id').html() + '<br />' + "<div class='div_hard_berry' onclick='"+nomFonction+"(\" " + data.results[i].url + " \")'>" + '#' + (i + 1) +  '</div>')
 
-            $('#pokemon_name').html( $('#pokemon_name').html() + '<br />' + "<div class='div_hard_berry' onclick='more_info_berry(\" " + data.results[i].url + " \")'>" + data.results[i].name + '</div>')
+            $('#pokemon_name').html( $('#pokemon_name').html() + '<br />' + "<div class='div_hard_berry' onclick='"+nomFonction+"(\" " + data.results[i].url + " \")'>" + data.results[i].name + '</div>')
             
         }
 
         if ( data.next !== null) {
 
             $('.next').css('display', 'block')
-            $('.next').attr('onclick', 'clicknext("' +data.next+ '")')
+            $('.next').attr('onclick', 'clicknext("' +data.next+ '","'+nomFonction+'")')
 
         }
 
@@ -377,7 +712,7 @@ function clicknext(url) {
         if ( data.previous !== null) {
 
             $('.previous').css('display', 'block')
-            $('.previous').attr('onclick', 'clickprevious("' +data.previous+ '")')
+            $('.previous').attr('onclick', 'clickprevious("' +data.previous+ '","'+nomFonction+'")')
 
         }
 
@@ -393,14 +728,6 @@ function clicknext(url) {
 
 function clickprevious(url) {
 
-    console.log(url)
-
-    let change_url = new URL(url).searchParams
-
-    let url_limit = change_url.get('limit')
-
-    console.log(url_limit)
-
     $.get(url, function(data) {
         
         $('#pokemon_id').html('')
@@ -410,16 +737,16 @@ function clickprevious(url) {
 
             // console.log(data.results[i])
 
-            $('#pokemon_id').html( $('#pokemon_id').html() + '<br />' + "<div class='div_hard_berry' onclick='more_info_berry(\" " + data.results[i].url + " \")'>" + '#' + (i + 1) +  '</div>')
+            $('#pokemon_id').html( $('#pokemon_id').html() + '<br />' + "<div class='div_hard_berry' onclick='"+nomFonction+"(\" " + data.results[i].url + " \")'>" + '#' + (i + 1) +  '</div>')
 
-            $('#pokemon_name').html( $('#pokemon_name').html() + '<br />' + "<div class='div_hard_berry' onclick='more_info_berry(\" " + data.results[i].url + " \")'>" + data.results[i].name + '</div>')
+            $('#pokemon_name').html( $('#pokemon_name').html() + '<br />' + "<div class='div_hard_berry' onclick='"+nomFonction+"(\" " + data.results[i].url + " \")'>" + data.results[i].name + '</div>')
 
         }
 
         if ( data.next !== null) {
 
             $('.next').css('display', 'block')
-            $('.next').attr('onclick', 'clicknext("' +data.next+ '")')
+            $('.next').attr('onclick', 'clicknext("' +data.next+ '","'+nomFonction+'")')
 
         }
 
@@ -432,7 +759,7 @@ function clickprevious(url) {
         if ( data.previous !== null) {
 
             $('.previous').css('display', 'block')
-            $('.previous').attr('onclick', 'clickprevious("' +data.previous+ '")')
+            $('.previous').attr('onclick', 'clickprevious("' +data.previous+ '","'+nomFonction+'")')
 
         }
 
